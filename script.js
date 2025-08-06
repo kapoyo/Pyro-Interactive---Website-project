ba39ee8f747bbb6102f2d415b254df4eef3da51e
kap <ordinarygamer534@gmail.com>
2025-08-06 16:25:06 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const navBar = `
+    <div class="header">
+        <nav>
+            <a class="nav-link" href="./home.html">Home</a>
+            <a class="nav-link" href="./ViraBlock.html">ViraBlock</a>
+            <a class="nav-link" href="./Encrypted-Realm.html">The Encrypted Realm</a>
+            <a class="nav-link" href="./Defend-Net.html">Defend the Net</a>
+        </nav>
+    </div>
+`
+
+document.addEventListener("DOMContentLoader" function(
+    {
+        const navBarPlaceholder = document.getElementById()
+    }
+)
+
+
+}
